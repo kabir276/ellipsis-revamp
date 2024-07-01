b67bbc11f51c7fe13512eb4064ec56d18d173a83
kabir276 <kchhabra276@gmail.com>
2024-07-02 04:59:28 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,8 @@ const Home = () => {
   return (
     <main className="relative bg-[#000] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-
-        <Hero />
         <FloatingNav navItems={navItems} />
+        <Hero />
         <LoomVideoFrame />
         <SlideInElement />
         <CardStack />
